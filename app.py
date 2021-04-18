9fd4f4c77d2e9d0de417b3426cf3e37a66ca4f05
Fitsum A <fitsum@gmail.com>
2021-04-18 15:41:34 +0300

    creating a panda frame

---
+++ app.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import datetime as dt
+import calendar
+import plotly
+import plotly.express as px
+
+#Files list 
+files = ['Jan.xlsx', 'Feb.xlsx', 'Mar.xlsx']
+
+# Merged 
+merged = pd.DataFrame()
