1966e41f5cadd89047ecf9299965156e9fe01e14
Fitsum A <fitsum@gmail.com>
2021-04-18 15:43:59 +0300

    looping and mergining the data set to one

--- app.py
+++ app.py
@@ -5,7 +5,16 @@ import plotly
 import plotly.express as px
 
 #Files list 
-files = ['Jan.xlsx', 'Feb.xlsx', 'Mar.xlsx']
+report_files = ['Jan.xlsx', 'Feb.xlsx', 'Mar.xlsx']
 
 # Merged 
 merged = pd.DataFrame()
+
+# The Loop for merging 
+for file in report_files:
+    df = pd.read_excel(file)
+    merged= merged.append(df,ignore_index = True)
+
+# export to excel 
+merged.to_excel('Sales_Q2020.xlsx', index = False, sheet_name='Quarter Sales Report')
+
